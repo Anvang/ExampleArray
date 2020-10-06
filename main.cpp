de57ed2a4dc835fe8e7056b57ff8a070d1539bc7
kbuffardi <kbuffardi@csuchico.edu>
2020-10-06 14:42:39 -0700

    Set up char array and string

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+const int LETTER_MAX = 10;
+
 int main()
 {
-  
+  char word[LETTER_MAX];
+  string dynamicWord;
+
+  for( int counter = 0; counter < 3; counter++)
+  {
+    cout << "Enter something:\n";
+    cin >> word;
+    dynamicWord = word;
+    cout << word << " is " << sizeof(word)/sizeof(word[0]) << endl;
+    cout << "The string version is " << dynamicWord.length() << endl;
+  }
+
   return 0;
 }
